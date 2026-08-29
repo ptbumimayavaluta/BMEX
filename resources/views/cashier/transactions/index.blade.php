64dee9e73e539bf289efa1ffe8c0c51b5b8b5f09
umarhidaayat <umarhidaayat@gmail.com>
2026-08-29 23:44:33 +0800

    fix

--- resources/views/cashier/transactions/index.blade.php
+++ resources/views/cashier/transactions/index.blade.php
@@ -340,21 +340,27 @@
 
     <script>
         function submitOverrideDttot() {
-            // 1. Ubah nilai override menjadi 1
-            let overrideInput = document.getElementById('dttotOverrideInput');
+            var form = document.getElementById('transactionForm');
+            var overrideInput = document.getElementById('dttotOverrideInput');
+            
             if (overrideInput) {
                 overrideInput.value = '1';
             }
             
-            // 2. Wajib bersihkan format ribuan di semua input amount_foreign agar lolos validasi numeric
-            let form = document.getElementById('transactionForm');
-            let inputs = form.querySelectorAll('input[name*="amount_foreign"]');
-            inputs.forEach(input => {
-                input.value = parseNumber(input.value); 
+            var inputs = form.querySelectorAll('input[name*="amount_foreign"]');
+            inputs.forEach(function(input) {
+                if (typeof parseNumber === "function") {
+                    input.value = parseNumber(input.value); 
+                } else {
+                    input.value = input.value.replace(/\./g, '').replace(/,/g, '.');
+                }
             });
 
-            // 3. Submit form transaksi
-            form.submit();
+            if (typeof form.requestSubmit === 'function') {
+                form.requestSubmit();
+            } else {
+                form.submit();
+            }
         }
     </script>
     @endif
@@ -952,34 +958,5 @@
             </div>
         </div>
     </div>
-
-    <script>
-        function submitOverrideDttot() {
-            var form = document.getElementById('transactionForm');
-            var overrideInput = document.getElementById('dttotOverrideInput');
-            
-            if (overrideInput) {
-                // 1. Set flag override dttot menjadi 1
-                overrideInput.value = '1';
-            }
-            
-            // 2. Bersihkan format ribuan titik/koma secara manual di semua baris valas
-            var inputs = form.querySelectorAll('input[name*="amount_foreign"]');
-            inputs.forEach(function(input) {
-                if (typeof parseNumber === "function") {
-                    input.value = parseNumber(input.value); 
-                } else {
-                    input.value = input.value.replace(/\./g, '').replace(/,/g, '.');
-                }
-            });
-
-            // 3. Gunakan requestSubmit() agar menembus listener validasi form
-            if (typeof form.requestSubmit === 'function') {
-                form.requestSubmit();
-            } else {
-                form.submit();
-            }
-        }
-    </script>
     @endif
 @endsection
